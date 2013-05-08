a166010e284d48fc86d9b0a7cda9ca6ac782157b
quimera <s.huens@gmail.com>
2013-05-08 13:53:24 -0300

    video gallery tile

--- src/brasil/gov/tiles/static/tiles.js
+++ src/brasil/gov/tiles/static/tiles.js
@@ -1,3 +1,50 @@
 /* 
   Comentario
 */
+$(document).ready(function() {
+    //video gallery
+    if ($('.videogallery-tile')[0] !== undefined) {
+        $('.videogallery-tile').each(function(){
+
+
+            //fake responsive (1, 2 or 3 columns)
+            var width = $(this).width();
+            // 2 column, 230
+            if (width > 280) {
+                $(this).addClass('gallery-2-columns');
+            }
+            // 3 columns, 460 + 30 padding
+            if (width > 500) {
+                $(this).removeClass('gallery-2-columns');
+                $(this).addClass('gallery-3-columns');
+            }
+
+            //resize video onload
+            var video_width = $('.player-holder iframe').width();
+            $('.player-holder iframe').height(video_width/1.3333);
+
+            //video loading logic
+            var video_gallery = $(this);
+            var gallery_link = video_gallery.find('.gallery-element').find('.gallery-element-link');
+            gallery_link.click(function(e){
+                e.preventDefault();
+
+                var player = $(this).data('player-dom');
+                var metadata = $(this).siblings('.gallery-element-metadata');
+
+                video_gallery.find('.selected').removeClass('selected');
+                $(this).parents('.gallery-element').addClass('selected');
+                
+                var player_slot = video_gallery.find('.player-holder');
+                player_slot.find('.player-video').html(player);
+                player_slot.find('.title').html(metadata.find('.title').html());
+                player_slot.find('.description').html(metadata.find('.description').html());
+
+                //resize video onload
+                var video_width = $('.player-holder iframe').width();
+                $('.player-holder iframe').height(video_width/1.3333);
+
+            });
+        });
+    }
+});
---
+++ src/brasil/gov/tiles/tiles/videogallery.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+from collective.cover import _
+from collective.cover.tiles.list import IListTile
+from collective.cover.tiles.list import ListTile
+from plone.tiles.interfaces import ITileDataManager
+from plone.uuid.interfaces import IUUID
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope import schema
+
+
+class IVideoGalleryTile(IListTile):
+    """
+    """
+
+    title = schema.TextLine(
+        title=_(u'Title'),
+        required=False,
+    )
+
+    subtitle = schema.TextLine(
+        title=_(u'Subtitle'),
+        required=False,
+        readonly=False,
+    )
+
+    footer_text = schema.TextLine(
+        title=_(u'Footer Link'),
+        required=False,
+        readonly=False,
+    )
+
+    uuids = schema.List(
+        title=_(u'Videos'),
+        value_type=schema.TextLine(),
+        required=False,
+        readonly=True,
+    )
+
+
+class VideoGalleryTile(ListTile):
+    index = ViewPageTemplateFile("templates/videogallery.pt")
+    is_configurable = False
+    is_editable = True
+
+    def populate_with_object(self, obj):
+        super(ListTile, self).populate_with_object(obj)  # check permission
+
+        #here we should check if the embeded item has its a video
+        # XXX
+
+        self.set_limit()
+        uuid = IUUID(obj, None)
+        data_mgr = ITileDataManager(self)
+
+        old_data = data_mgr.get()
+        old_data['uuids'] = [uuid]
+        data_mgr.set(old_data)
+
+    def get_uid(self, obj):
+        return IUUID(obj)
+
+    def thumbnail(self, item):
+        scales = item.restrictedTraverse('@@images')
+        try:
+            return scales.scale('image', width=80, height=60)
+        except:
+            return None
+
+    def accepted_ct(self):
+        """ Return a list of content types accepted by the tile.
+        """
+        return ['Collection', 'Folder']
+
+    def get_elements(self, obj):
+        results = []
+        if obj:
+            portal_type = obj.getPortalTypeName()
+
+            catalog_results = []
+            if portal_type == 'Collection':
+                catalog_results = obj.results()
+            elif portal_type == 'Folder':
+                catalog_results = obj.getFolderContents({"portal_type": "sc.embedder"})
+
+            if catalog_results:
+                for i in xrange(self.limit):
+                    results.append(catalog_results[i].getObject())
+
+        return results
