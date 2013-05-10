d444165492786845de15af00bc0ffeeb71b08913
hvelarde <hector.velarde@gmail.com>
2013-05-09 21:26:50 -0300

    update NITF tile

--- src/brasil/gov/tiles/tests/test_nitf_tile.py
+++ src/brasil/gov/tiles/tests/test_nitf_tile.py
@@ -8,11 +8,6 @@ from zope.interface.verify import verifyObject
 
 import unittest
 
-# XXX: remove this when release available
-import pkg_resources
-nitf_available = 'collective.nitf' in pkg_resources.AvailableDistributions()
-msg = "collective.nitf not installed; skipping test"
-
 
 class NITFBasicTileTestCase(BaseIntegrationTestCase):
 
@@ -21,7 +16,6 @@ class NITFBasicTileTestCase(BaseIntegrationTestCase):
         self.tile = self.portal.restrictedTraverse(
             '@@%s/%s' % ('nitf.basic', 'test-tile'))
 
-    @unittest.skipUnless(nitf_available, msg)
     def test_interface(self):
         self.assertTrue(IPersistentCoverTile.implementedBy(NITFBasicTile))
         self.assertTrue(verifyClass(IPersistentCoverTile, NITFBasicTile))
@@ -30,13 +24,11 @@ class NITFBasicTileTestCase(BaseIntegrationTestCase):
         self.assertTrue(IPersistentCoverTile.providedBy(tile))
         self.assertTrue(verifyObject(IPersistentCoverTile, tile))
 
-    @unittest.skipUnless(nitf_available, msg)
     def test_default_configuration(self):
         self.assertTrue(self.tile.is_configurable)
         self.assertTrue(self.tile.is_editable)
         self.assertTrue(self.tile.is_droppable)
 
-    @unittest.skipUnless(nitf_available, msg)
     def test_accepted_content_types(self):
         self.assertListEqual(
             self.tile.accepted_ct(), ['collective.nitf.content'])
