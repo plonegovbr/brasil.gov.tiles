3deac682823459fe42ba7bcaf8794c3c73b21762
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2013-08-28 09:47:38 -0300

    Adicionado título no audio gallery

--- src/brasil/gov/tiles/tiles/audiogallery.py
+++ src/brasil/gov/tiles/tiles/audiogallery.py
@@ -13,6 +13,11 @@ class IAudioGalleryTile(IListTile):
     """
     """
 
+    header = schema.TextLine(
+        title=_(u'Header'),
+        required=False,
+    )
+
     title = schema.TextLine(
         title=_(u'Title'),
         required=False,
@@ -44,10 +49,12 @@ class AudioGalleryTile(ListTile):
         # XXX
 
         self.set_limit()
+        header = obj.Title()  # use collection's title as header
         uuid = IUUID(obj, None)
         data_mgr = ITileDataManager(self)
 
         old_data = data_mgr.get()
+        old_data['header'] = header
         old_data['uuids'] = [uuid]
         data_mgr.set(old_data)
 
@@ -79,6 +86,9 @@ class AudioGalleryTile(ListTile):
                     results.append(catalog_results[i].getObject())
 
         return results
+
+    def show_header(self):
+        return self._field_is_visible('header')
 
     def init_js(self):
         return """
