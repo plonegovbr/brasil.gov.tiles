dc516885631d31b4bf0ef998650d54d46ee452ee
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2013-08-29 16:30:39 -0300

    Trazendo tile embed para o brasil.gov.tiles

---
+++ src/brasil/gov/tiles/tiles/embed.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from collective.cover import _
+from collective.cover.tiles.base import IPersistentCoverTile
+from collective.cover.tiles.base import PersistentCoverTile
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope import schema
+from zope.interface import implements
+
+
+class IEmbedTile(IPersistentCoverTile):
+
+    embed = schema.Text(
+        title=_(u'Embedding code'),
+        required=False,
+    )
+
+    title = schema.TextLine(
+        title=_(u'Title'),
+        required=False,
+    )
+
+    description = schema.Text(
+        title=_(u'Description'),
+        required=False,
+    )
+
+
+class EmbedTile(PersistentCoverTile):
+
+    implements(IEmbedTile)
+    index = ViewPageTemplateFile('templates/embed.pt')
+
+    is_configurable = True
+    is_editable = True
+    is_droppable = False
+
+    def is_empty(self):
+        return not (self.data.get('embed', None) or
+                    self.data.get('title', None) or
+                    self.data.get('description', None))
