4d9c09fef02f38a92e886624ac0181f08ca5b87e
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2013-09-30 14:38:36 -0300

    Modificada herança para patch

--- src/brasil/gov/tiles/tiles/audio.py
+++ src/brasil/gov/tiles/tiles/audio.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from brasil.gov.tiles.tiles.base import IPersistentCoverTile
-from brasil.gov.tiles.tiles.base import PersistentCoverTile
 from collective.cover import _
+from collective.cover.tiles.base import IPersistentCoverTile
+from collective.cover.tiles.base import PersistentCoverTile
 from plone.tiles.interfaces import ITileDataManager
 from plone.uuid.interfaces import IUUID
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
