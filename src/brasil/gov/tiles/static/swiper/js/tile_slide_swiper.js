23197f46ae945c7ee818eaa0a97d224f9ca7512b
Jefferson Almeida <jralmeida88@gmail.com>
2018-03-09 11:55:36 -0300

    Create new tile Swiper hardcode

---
+++ src/brasil/gov/tiles/static/swiper/js/tile_slide_swiper.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+	//initialize swiper when document ready
+	var mySwiper = new Swiper ('.swiper-container', {
+	  // Optional parameters
+	  direction: 'horizontal',
+	  loop: true
+	})
+});
