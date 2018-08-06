2dc8676bf059dc3e6c9443518fc875cf2c7b339c
hvelarde <hector.velarde@gmail.com>
2018-08-06 18:55:39 -0300

    Fix upgrade steps for branch 2.x

--- src/brasil/gov/tiles/tests/test_upgrades.py
+++ src/brasil/gov/tiles/tests/test_upgrades.py
@@ -1,46 +1,58 @@
 # -*- coding: utf-8 -*-
 from brasil.gov.tiles.testing import INTEGRATION_TESTING
-from collective.cover.config import IS_PLONE_5
 from collective.cover.controlpanel import ICoverSettings
-from collective.cover.tests.test_upgrades import UpgradeTestCaseBase
 from plone import api
 
 import unittest
 
 
-class UpgradeTestCaseBrasilGovTitles(UpgradeTestCaseBase):
-    """
-    Classe que altera o profile_id para 'brasil.gov.tiles:default'
-    """
-
-    def setUp(self, from_version, to_version):
-        UpgradeTestCaseBase.setUp(self, from_version, to_version)
-        self.profile_id = u'brasil.gov.tiles:default'
-
-
-# FIXME: move all steps to 4100
-class Upgrade4004to4005TestCase(UpgradeTestCaseBrasilGovTitles):
+class BaseUpgradeTestCase(unittest.TestCase):
 
     layer = INTEGRATION_TESTING
+    profile_id = u'brasil.gov.tiles:default'
 
     def setUp(self):
-        super(Upgrade4004to4005TestCase, self).setUp(u'4004', u'4005')
+        self.portal = self.layer['portal']
+        self.setup = self.portal['portal_setup']
+        self.setup.setLastVersionForProfile(self.profile_id, self.from_)
 
-    def test_registrations(self):
+    def _get_upgrade_step_by_title(self, title):
+        """Return the upgrade step that matches the title specified."""
+        self.setup.setLastVersionForProfile(self.profile_id, self.from_)
+        upgrades = self.setup.listUpgrades(self.profile_id)
+        steps = [s for s in upgrades[0] if s['title'] == title]
+        return steps[0] if steps else None
+
+    def _do_upgrade(self, step):
+        """Execute an upgrade step."""
+        request = self.layer['request']
+        request.form['profile_id'] = self.profile_id
+        request.form['upgrades'] = [step['id']]
+        self.setup.manage_doUpgrades(request=request)
+
+
+class UpgradeTo4100TestCase(BaseUpgradeTestCase):
+
+    from_ = u'*'
+    to_ = u'4100'
+
+    def test_profile_version(self):
         version = self.setup.getLastVersionForProfile(self.profile_id)[0]
-        self.assertTrue(int(version) >= int(self.to_version))
-        self.assertEqual(self._how_many_upgrades_to_do(), 2)
+        self.assertEqual(version, self.from_)
 
-    @unittest.expectedFailure
+    def test_registered_steps(self):
+        steps = len(self.setup.listUpgrades(self.profile_id)[0])
+        self.assertEqual(steps, 4)
+
     def test_update_resources_references(self):
         # address also an issue with Setup permission
-        title = u'Use resource compiled from webpack'
-        step = self._get_upgrade_step(title)
+        title = u'Use resources compiled from webpack'
+        step = self._get_upgrade_step_by_title(title)
         self.assertIsNotNone(step)
 
         # simulate state on previous version
-        from brasil.gov.tiles.upgrades.v4005 import _rename_resources
-        from brasil.gov.tiles.upgrades.v4005 import RESOURCES_TO_UPDATE
+        from brasil.gov.tiles.upgrades.v4100 import _rename_resources
+        from brasil.gov.tiles.upgrades.v4100 import RESOURCES_TO_UPDATE
         RESOURCES_TO_UPDATE_INVERSE = {v: k for k, v in RESOURCES_TO_UPDATE.items()}
 
         css_tool = api.portal.get_tool('portal_css')
@@ -64,7 +76,7 @@ class Upgrade4004to4005TestCase(UpgradeTestCaseBrasilGovTitles):
         self.assertNotIn('++resource++brasil.gov.tiles/vendor/jquery.jplayer.min.js', js_ids)
 
         # run the upgrade step to validate the update
-        self._do_upgrade_step(step)
+        self._do_upgrade(step)
 
         css_ids = css_tool.getResourceIds()
         self.assertIn('++resource++brasil.gov.tiles/brasilgovtiles.css', css_ids)
@@ -80,23 +92,9 @@ class Upgrade4004to4005TestCase(UpgradeTestCaseBrasilGovTitles):
         self.assertNotIn('++resource++brasil.gov.tiles/jquery.cycle2.js', js_ids)
         self.assertNotIn('++resource++brasil.gov.tiles/jquery.jplayer.min.js', js_ids)
 
-
-class UpgradeTo4100TestCase(UpgradeTestCaseBrasilGovTitles):
-
-    layer = INTEGRATION_TESTING
-
-    def setUp(self):
-        super(UpgradeTo4100TestCase, self).setUp(u'*', u'4100')
-
-    def test_registrations(self):
-        version = self.setup.getLastVersionForProfile(self.profile_id)[0]
-        self.assertTrue(int(version) >= int(self.to_version))
-        self.assertEqual(self._how_many_upgrades_to_do(), 2)
-
-    @unittest.skipIf(IS_PLONE_5, 'Upgrade step not supported under Plone 5')
     def test_add_quote_tile(self):
         title = u'Add Quote tile'
-        step = self._get_upgrade_step(title)
+        step = self._get_upgrade_step_by_title(title)
         self.assertIsNotNone(step)
 
         tile = u'brasil.gov.tiles.quote'
@@ -110,7 +108,7 @@ class UpgradeTo4100TestCase(UpgradeTestCaseBrasilGovTitles):
         self.assertNotIn(tile, available_tiles)
 
         # run the upgrade step to validate the update
-        self._do_upgrade_step(step)
+        self._do_upgrade(step)
 
         name = 'plone.app.tiles'
         registered_tiles = api.portal.get_registry_record(name=name)
--- src/brasil/gov/tiles/upgrades/v4100/__init__.py
+++ src/brasil/gov/tiles/upgrades/v4100/__init__.py
@@ -1,5 +1,33 @@
 # -*- coding: utf-8 -*-
+from brasil.gov.tiles.logger import logger
 from brasil.gov.tiles.upgrades import add_tile
+from plone import api
+
+
+RESOURCES_TO_UPDATE = {
+    '++resource++brasil.gov.tiles/tiles.css': '++resource++brasil.gov.tiles/brasilgovtiles.css',
+    '++resource++brasil.gov.tiles/tiles.js': '++resource++brasil.gov.tiles/brasilgovtiles.js',
+    '++resource++brasil.gov.tiles/jquery.cycle2.carousel.js': '++resource++brasil.gov.tiles/vendor/jquery.cycle2.carousel.js',
+    '++resource++brasil.gov.tiles/jquery.cycle2.js': '++resource++brasil.gov.tiles/vendor/jquery.cycle2.js',
+    '++resource++brasil.gov.tiles/jquery.jplayer.min.js': '++resource++brasil.gov.tiles/vendor/jquery.jplayer.min.js',
+}
+
+
+def _rename_resources(tool, from_to):
+    for id_ in tool.getResourceIds():
+        if id_ in from_to:
+            tool.renameResource(id_, from_to[id_])
+
+
+def update_static_resources(setup_tool):
+    """Fix resource references after static files reorganization."""
+    css_tool = api.portal.get_tool('portal_css')
+    _rename_resources(css_tool, RESOURCES_TO_UPDATE)
+    logger.info('CSS resources were updated')
+
+    js_tool = api.portal.get_tool('portal_javascripts')
+    _rename_resources(js_tool, RESOURCES_TO_UPDATE)
+    logger.info('JavaScript resources were updated')
 
 
 def add_quote_tile(context):
