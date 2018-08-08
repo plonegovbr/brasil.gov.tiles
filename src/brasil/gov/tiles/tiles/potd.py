ea38a89e9e833eb7a4206e0edaf06067dc9a99c0
Clayton Caetano de Sousa <claytonc.sousa@gmail.com>
2018-08-08 00:17:57 -0300

    refatoração no código.

--- src/brasil/gov/tiles/tiles/potd.py
+++ src/brasil/gov/tiles/tiles/potd.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from Acquisition import aq_base
 from brasil.gov.tiles import _
 from collective.cover.tiles.base import IPersistentCoverTile
 from collective.cover.tiles.base import PersistentCoverTile
@@ -7,7 +6,7 @@ from plone import api
 from plone.namedfile import field
 from plone.namedfile import NamedBlobImage
 from plone.tiles.interfaces import ITileDataManager
-from Products.CMFPlone.utils import safe_unicode
+from Products.CMFPlone.utils import safe_hasattr
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from zope import schema
 from zope.interface import implementer
@@ -64,27 +63,25 @@ class POTDTile(PersistentCoverTile):
     def populate_with_object(self, obj):
         super(POTDTile, self).populate_with_object(obj)
 
-        obj = aq_base(obj)
-        title = obj.Title()
-        rights = obj.Rights()
-        description = obj.Description()
-        img = obj.image.data
-        uuid = api.content.get_uuid(obj)
-
-        if img:
+        if safe_hasattr(obj, 'getImage'):
+            data = obj.getImage().data
+        else:
             data = obj.image.data
+
+        if data:
             image = NamedBlobImage(data)
         else:
             image = None
 
         data_mgr = ITileDataManager(self)
-        data_mgr.set({
-            'title': safe_unicode(title),
-            'image': image,
-            'description': safe_unicode(description),
-            'photo_credits': safe_unicode(rights),
-            'uuid': uuid,
-        })
+        data = data_mgr.get()
+        data['title'] = obj.title
+        data['description'] = obj.description
+        data['image'] = image
+        data['uuid'] = api.content.get_uuid(obj=obj)
+        data['photo_credits'] = obj.Rights()
+
+        data_mgr.set(data)
 
     @property
     def is_empty(self):
