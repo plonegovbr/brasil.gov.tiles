85c557006cf79157787f69b33c8302e3c81c1d5d
hvelarde <hector.velarde@gmail.com>
2018-09-04 13:56:26 -0300

    Fix package classifiers
    We're in beta now.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="Tiles para o Portal Padrão do Governo Federal",
     long_description=long_description,
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
